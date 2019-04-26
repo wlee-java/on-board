6d01d856527cf5a860c5f4c36b0fa13236036427
W Lee <wolee1977@gmail.com>
2019-04-25 20:15:11 -0400

    Initial commit.

--- src/main/java/com/cwt/bpg/onboard/Application.java
+++ src/main/java/com/cwt/bpg/onboard/Application.java
@@ -2,7 +2,7 @@ package com.cwt.bpg.onboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//random comment
 @SpringBootApplication
 public class Application {
 
